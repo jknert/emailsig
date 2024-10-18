9d083598d3abf05aa20c434201776e955dcc986e
jknert <jacob@venhub.com>
2024-10-18 11:55:09 -0700

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ document.getElementById('signature-form').addEventListener('submit', function (e
     // Phone number is constant
     const phone = '888-585-4999';
 
-    // Disclaimer text
+    // Disclaimer text (Full text without abbreviation)
     const disclaimer = `This email and any files transmitted with it are confidential and intended solely for the use of the individual or entity to whom they are addressed. If you have received this email in error please notify the system manager. This message contains confidential information and is intended only for the individual named. If you are not the named addressee you should not disseminate, distribute or copy this e-mail. Please notify the sender immediately by e-mail if you have received this e-mail by mistake and delete this e-mail from your system. If you are not the intended recipient you are notified that disclosing, copying, distributing or taking any action in reliance on the contents of this information is strictly prohibited. If you are not the intended recipient you are notified that disclosing, copying, distributing or taking any action in reliance on the contents of this information is strictly prohibited. Nothing in this email should be construed as an agreement, or intent to agree, or advise, or representation of the company’s position, or releasing any confidential information, can be used as any form of evidence. This email is strictly as a basic form of communication and legal standing or official communication, all commitments, official communications, company’s position with regards to any terms of communication must be in a form of a contract along with a non-disclosure agreement and non-compete agreement. Should you disagree with any of the above, please notify the sender within 24 hours from receipt. As you read this email and not notify the sender of your objection will be an acceptance by you.`;
 
     // Email signature HTML template with placeholders
@@ -21,7 +21,7 @@ document.getElementById('signature-form').addEventListener('submit', function (e
             <!-- Logo -->
             <tr>
                 <td style="text-align: left;">
-                    <img src="https://vhphoto.s3.us-east-2.amazonaws.com/Venhub+Logo+with+Black+-+Red+dot+no+background.png" alt="VenHub Logo" width="113" height="30" style="border:0;">
+                    <img src="https://vhphoto.s3.us-east-2.amazonaws.com/Venhub+Logo+with+Black+-+Red+dot+no+background.png" alt="VenHub Logo" width="113" height="27" style="border:0;">
                 </td>
             </tr>
             <!-- Spacer below the logo -->
@@ -58,14 +58,42 @@ document.getElementById('signature-form').addEventListener('submit', function (e
                 <td style="padding-top: 19px; text-align: left;">
                     <table cellpadding="0" cellspacing="0" border="0" style="margin: 0;">
                         <tr>
-                            <!-- Update each icon with width and height attributes -->
+                            <!-- Website Icon -->
                             <td style="padding-right: 5px;">
                                 <a href="https://www.venhub.com" target="_blank">
                                     <img src="https://vhphoto.s3.us-east-2.amazonaws.com/Website+Icon.png" alt="Website" width="18" height="18" style="border:0;">
                                 </a>
                             </td>
-                            <!-- Repeat for other icons -->
-                            <!-- ... -->
+                            <!-- Pre-Order Icon -->
+                            <td style="padding-right: 5px;">
+                                <a href="https://www.venhub.com/pre-order-main" target="_blank">
+                                    <img src="https://vhphoto.s3.us-east-2.amazonaws.com/Pre+order+Icon.png" alt="Pre-Order" width="18" height="18" style="border:0;">
+                                </a>
+                            </td>
+                            <!-- Facebook Icon -->
+                            <td style="padding-right: 5px;">
+                                <a href="https://www.facebook.com/people/Venhub/100090915533691/" target="_blank">
+                                    <img src="https://vhphoto.s3.us-east-2.amazonaws.com/Facebook.png" alt="Facebook" width="18" height="18" style="border:0;">
+                                </a>
+                            </td>
+                            <!-- Twitter Icon -->
+                            <td style="padding-right: 5px;">
+                                <a href="https://twitter.com/Venhubai" target="_blank">
+                                    <img src="https://vhphoto.s3.us-east-2.amazonaws.com/x.png" alt="Twitter" width="18" height="18" style="border:0;">
+                                </a>
+                            </td>
+                            <!-- YouTube Icon -->
+                            <td style="padding-right: 5px;">
+                                <a href="https://youtube.com/@Venhub" target="_blank">
+                                    <img src="https://vhphoto.s3.us-east-2.amazonaws.com/YouTube.png" alt="YouTube" width="18" height="18" style="border:0;">
+                                </a>
+                            </td>
+                            <!-- LinkedIn Icon -->
+                            <td>
+                                <a href="https://www.linkedin.com/company/venhubai/" target="_blank">
+                                    <img src="https://vhphoto.s3.us-east-2.amazonaws.com/linkedin.png" alt="LinkedIn" width="18" height="18" style="border:0;">
+                                </a>
+                            </td>
                         </tr>
                     </table>
                 </td>
